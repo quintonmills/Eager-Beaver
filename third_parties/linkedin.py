8e3ef885468b1aa36d8297d52d565c100f76d670
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2024-03-30 10:04:54 -0400

    supply bearer token
    supply bearer authorization token to proxy curl. so that we can use their api endpoint to scrape LinkedIn data

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -5,6 +5,7 @@ def scrape_linkedin_profile(linkedin_profile_url)
     """scrape information from linkedin profiles,
     manually scrape the information from the linkedin profile"""
     api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
 
     response = requests.get(
         api_endpoint, params = {"url": linkedin_profile_url}, headers = header_dic
